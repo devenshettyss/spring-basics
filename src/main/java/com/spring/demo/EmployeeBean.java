b556882627eba5488950b91fb4cd6fbe44da72e7
devenshettyss <practice.devenshetty@gmail.com>
2021-08-24 05:46:34 +0000

    spring basics

---
+++ src/main/java/com/spring/demo/EmployeeBean.java
@@ -0,0 +1,21 @@
+package com.spring.demo;
+
+public class EmployeeBean {
+	private String fulName;
+	private DepartmentBean departmentBean;
+	public String getFulName() {
+		return fulName;
+	}
+	public void setFulName(String fulName) {
+		this.fulName = fulName;
+	}
+	public DepartmentBean getDepartmentBean() {
+		return departmentBean;
+	}
+	public void setDepartmentBean(DepartmentBean departmentBean) {
+		this.departmentBean = departmentBean;
+	}
+	
+	
+
+}
